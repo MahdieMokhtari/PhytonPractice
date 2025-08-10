a0d3e9cdb74ec0f997609868d5e9d8004f164f7d
MahdieMokhtari <mahdiehmokht@gmail.com>
2025-08-10 21:16:50 +0330

    Create passwordValidation.py

---
+++ passwordValidation.py
@@ -0,0 +1,15 @@
+def passValidation(yourPass):
+    if len(yourPass) < 8:
+        print("your pass must have at least 8 char! ")
+    elif yourPass.isalpha():
+        print("your pass must have at least 1 number! ")
+    elif yourPass.isalnum():
+        print("your pass must have at least 1 special char! ")
+    else:
+        print("your pass has accepted..! ")
+    return
+
+while True:
+    yourPass = input("enter your pass: ")
+    passValidation(yourPass)
+    break
